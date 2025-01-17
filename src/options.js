c5f07317f171c6e52094f94acb9297c0715abc94
Harry P <32145066+papaharry@users.noreply.github.com>
2025-01-17 10:49:12 +0000

    [ISSUES-28] - fix(count): Revert count and map smart labels (#29)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- src/options.js
+++ src/options.js
@@ -12,8 +12,21 @@ function saveOptions(event) {
     event.preventDefault();
     const label = document.getElementById('label').value.trim();
 
-    // Transform "important" to "^iim" without reassigning label
-    const finalLabel = label.toLowerCase() === 'important' ? '^iim' : label;
+    // Define a mapping of labels to their transformed values
+    // the transformed values are the ones used and displayed in Gmail's Atom feed
+    // the original labels are the ones still used by good ol' Gmail although they updated the UI to the new values
+    const labelMap = {
+        'important': '^iim',
+        'personal': '^smartlabel_personal',
+        'social': '^smartlabel_social',
+        'promotions': '^smartlabel_promo',
+        'updates': '^smartlabel_notification',
+        'forums': '^smartlabel_group',
+        'newsletter': '^smartlabel_newsletter',
+        'receipt': '^smartlabel_receipt',
+    };
+
+    const finalLabel = labelMap[label.toLowerCase()] || label;
 
     chrome.storage.sync.set({ label: finalLabel }, () => {
         const status = document.getElementById('status');
